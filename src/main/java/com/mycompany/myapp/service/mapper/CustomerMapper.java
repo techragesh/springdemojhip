928be960bed04fce069cc80dfe38864ac6564547
ragesh.shunmugam <rshunmugam@FIT-LT00159.lan.khronos.nl>
2020-12-17 13:01:57 +0100

    intial commit

---
+++ src/main/java/com/mycompany/myapp/service/mapper/CustomerMapper.java
@@ -0,0 +1,25 @@
+package com.mycompany.myapp.service.mapper;
+
+
+import com.mycompany.myapp.domain.*;
+import com.mycompany.myapp.service.dto.CustomerDTO;
+
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity {@link Customer} and its DTO {@link CustomerDTO}.
+ */
+@Mapper(componentModel = "spring", uses = {})
+public interface CustomerMapper extends EntityMapper<CustomerDTO, Customer> {
+
+
+
+    default Customer fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Customer customer = new Customer();
+        customer.setId(id);
+        return customer;
+    }
+}
